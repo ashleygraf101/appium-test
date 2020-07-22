6b1edd5efaddc81e7aabcdd142e823644b2b3b35
ashleygraf101 <ashley.graf101@gmail.com>
2020-07-22 17:49:43 +1000

    learning more about appium - tests don't serve much purpose yet but they recognise elements

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ testing event listeners on the STN Android App
 
 1. Install the require Appium dependencies 
 2. Run ``npm install``
-3. Start Appium
+3. Start Appium server
 4. Start emulator
 5. Run ``node index.js``
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const wdio = require("webdriverio");
+var By = require('webdriverio').By;
 var assert = require('assert');
 
 const opts = {
@@ -10,8 +11,7 @@ const opts = {
     deviceName: "Android Emulator",
     // must be absolute path
     app: "C:/Users/ashle/say-their-names-android/app/build/outputs/apk/debug/app-debug.apk",
-    // app: "C:/Users/ashle/appium-test/ApiDemos-debug.apk",
-    // appPackage: "io.appium.android.apis",
+    appPackage: "io.saytheirnames.android",
     appActivity: "io.saytheirnames.activity.MainActivity",
     automationName: "UiAutomator2"
   }
@@ -19,27 +19,27 @@ const opts = {
 
 async function main () {
   const client = await wdio.remote(opts);
+  const field = await client.$("android.widget.TextView");
+  const value = await field.getText();
+  assert.equal(value,"SAY THEIR NAMES");
 
-/** donations button */
-//  //android.widget.FrameLayout[@content-desc="DONATIONS"]/android.view.ViewGroup/android.widget.TextView
-/** petitions button */
-//  //android.widget.FrameLayout[@content-desc="PETITIONS"]/android.view.ViewGroup/android.widget.TextView
-/** about button */
-//  //android.widget.FrameLayout[@content-desc="ABOUT"]/android.view.ViewGroup/android.widget.TextView
-/** home button */
-//  //android.widget.FrameLayout[@content-desc="HOME"]/android.view.ViewGroup/android.widget.TextView
-/** profile pieces */
-//  /hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[2]/android.view.ViewGroup/android.widget.ImageView
-//  /hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[2]/android.view.ViewGroup/android.widget.TextView[1]
-//  /hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[2]/android.view.ViewGroup/android.widget.TextView[2]
-/** header image */
-//  /hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup[2]/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout/androidx.viewpager.widget.ViewPager/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout/android.widget.FrameLayout
+  const field1 = await client.$('//*[@content-desc="DONATIONS"]/android.view.ViewGroup/android.widget.TextView');
+  const value1 = await field1.getText();
+  assert.equal(value1,"DONATIONS");
 
+  const field2 = await client.$('//*[@content-desc="PETITIONS"]/android.view.ViewGroup/android.widget.TextView');
+  const value2 = await field2.getText();
+  assert.equal(value2,"PETITIONS");
 
+  const field3 = await client.$('//*[@content-desc="ABOUT"]/android.view.ViewGroup/android.widget.TextView');
+  const value3 = await field3.getText();
+  assert.equal(value3,"ABOUT");
 
+  const field4 = await client.$('//*[@content-desc="HOME"]/android.view.ViewGroup/android.widget.TextView');
+  const value4 = await field4.getText();
+  assert.equal(value4,"HOME");
 
-
- await client.deleteSession();
+  await client.deleteSession();
 }
 
 main();
